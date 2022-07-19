95a1b77502662a4d043377cf8323ddbb8b2b550e
Code2Riches <christian.benites@codeimmersives.com>
2022-07-19 15:45:29 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// Creates Div
+let div = $('<div>Hello World!</div>')
+
+let body = $('body');
+
+//adds our div to the body
+body.append(div);
+
+$('div').on('click', () => alert("Hello World!"));
+
+// div.on('click', function(){
+//     alert("Hello World!");
+// })
